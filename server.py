f6d7fa58cccb8b5fcba9c5ee588d8fc39343f273
wldhks1959 <gg812056@gmail.com>
2024-06-29 16:20:34 +0900

    5강

--- server.py
+++ server.py
@@ -3,9 +3,29 @@ import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id':1, 'title':'html', 'body':'html is ...'},
+    {'id':2, 'title':'css', 'body':'css is ...'},
+    {'id':3, 'title':'javascript', 'body':'javascript is ...'}
+]
+
 @app.route('/')
 def index():
-    return 'random : <strong>' + str(random.random()) + '</strong>'
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
 
 @app.route('/create/')
 def create():
