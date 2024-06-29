1c2b38015b7c0e946b151f02df06928e2dbf85f0
wldhks1959 <gg812056@gmail.com>
2024-06-29 16:51:35 +0900

    6강

--- server.py
+++ server.py
@@ -9,23 +9,41 @@ topics = [
     {'id':3, 'title':'javascript', 'body':'javascript is ...'}
 ]
 
-@app.route('/')
-def index():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+def template(contents, content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a></h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
         </body>
     </html>
     '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')
+def reader(id):
+    print(type(id))
+    title=''
+    body=''
+    for topic in topics:
+        if id==topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    print(title,body)
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
 @app.route('/create/')
 def create():
